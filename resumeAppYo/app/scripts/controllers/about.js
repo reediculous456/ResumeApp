c82acc5cc5d0e9316a7ba3e4c5e6ff3aa57e739f
Wesley Reed <reedws@ucmail.uc.edu>
2018-09-20 14:52:24 -0400

    created project with yeoman as test

---
+++ resumeAppYo/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resumeappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the resumeappApp
+ */
+angular.module('resumeappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ resumeAppYo/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resumeappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the resumeappApp
+ */
+angular.module('resumeappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
